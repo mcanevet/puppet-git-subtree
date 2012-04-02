7fc9abf2d11a09f2092589e102d68ffe163122a1
Raphaël Pinson <raphael.pinson@camptocamp.com>
2012-04-02 17:34:00 +0200

    Add bash completion to git-subtree. Fix syntax as per puppet-lint.

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,22 +7,29 @@ Installs git-subtree and adds a symlink to /usr/local/bin
 */
 class git-subtree {
 
-  if (versioncmp("1.6.999", $git_version) > 0) {
-    fail "git-subtree requires git 1.7 or later!"
+  if (versioncmp('1.6.999', $::git_version) > 0) {
+    fail 'git-subtree requires git 1.7 or later!'
   }
 
-  vcsrepo { "/usr/src/git-subtree":
+  vcsrepo { '/usr/src/git-subtree':
     ensure   => present,
-    source   => "http://github.com/apenwarr/git-subtree.git",
+    source   => 'http://github.com/apenwarr/git-subtree.git',
     provider => 'git',
     revision => '2793ee6ba',
   }
 
-  file { "/usr/local/bin/git-subtree":
+  file { '/usr/local/bin/git-subtree':
     ensure  => file,
-    source  => "file:///usr/src/git-subtree/git-subtree.sh",
-    owner   => "root",
-    mode    => 0755,
-    require => Vcsrepo["/usr/src/git-subtree"],
+    source  => 'file:///usr/src/git-subtree/git-subtree.sh',
+    owner   => 'root',
+    mode    => '0755',
+    require => Vcsrepo['/usr/src/git-subtree'],
+  }
+
+  file { '/etc/bash_completion.d/git-subtree':
+    ensure => file,
+    # Use /usr/src once PR 12 is merged by upstream
+    source => 'puppet:///modules/git-subtree/bash_completion.sh',
+    mode   => '0644',
   }
 }
